fb27204d94578a893807372c98371de7593825b7
Rushabh Koletiwar <155234426+Rushabh1310@users.noreply.github.com>
2024-10-04 14:36:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2351-number-of-ways-to-buy-pens-and-pencils/2351-number-of-ways-to-buy-pens-and-pencils.java
+++ 2351-number-of-ways-to-buy-pens-and-pencils/2351-number-of-ways-to-buy-pens-and-pencils.java
@@ -1,9 +1,14 @@
 class Solution {
     public long waysToBuyPensPencils(int total, int cost1, int cost2) {
-        long ans = 0;
-        for (int pens = 0; pens <= total / cost1; pens++) {
-            int remainingMoney = total - pens * cost1;
-            ans += (remainingMoney / cost2) + 1;
+        long ans = 1;
+        if (cost1 > total && cost2 > total) {
+            return 1;
+        }
+        int pens = total / cost1;
+        for (int i = 0; i <= pens; i++) {
+            int remaning = total - (i * cost1);
+            int pencils = remaning / cost2;
+            ans += pencils + 1;
         }
         return ans;
     }
