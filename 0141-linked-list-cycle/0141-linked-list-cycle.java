363773de4742c38c6e3457498fdc6bd9916df56b
Rushabh Koletiwar <155234426+Rushabh1310@users.noreply.github.com>
2024-10-18 14:48:27 +0530

    Time: 0 ms (100%), Space: 44.3 MB (65.09%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        // Floyd's Cycle-Finding Algorithm (also known as the Tortoise and Hare algorithm)
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true; // If fast and slow pointers meet, a cycle exists
+            }
+        }
+        return false; // If we reach the end of the list, there is no cycle
+    }
+}
