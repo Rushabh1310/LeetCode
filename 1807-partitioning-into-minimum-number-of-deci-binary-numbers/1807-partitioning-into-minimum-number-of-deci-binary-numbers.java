61929fcdf171aa75966ebf5da4a7d367fd2db9f0
Rushabh Koletiwar <155234426+Rushabh1310@users.noreply.github.com>
2024-11-20 12:19:06 +0530

    Time: 4 ms (91.4%), Space: 45 MB (66.35%) - LeetHub

--- 1807-partitioning-into-minimum-number-of-deci-binary-numbers/1807-partitioning-into-minimum-number-of-deci-binary-numbers.java
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/1807-partitioning-into-minimum-number-of-deci-binary-numbers.java
@@ -3,7 +3,7 @@ class Solution {
         int max = Integer.MIN_VALUE;
         for (char c : n.toCharArray()) {
             if ((c - '0') > max) {
-                max = c- '0';
+                max = c - '0';
             }
         }
         return max;
