6ab2e927f8c5786502eafd3561695ea1c74677cc
Rushabh Koletiwar <155234426+Rushabh1310@users.noreply.github.com>
2024-11-27 14:34:41 +0530

    Time: 16 ms (43.67%), Space: 42.7 MB (19.14%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -5,18 +5,16 @@ class Solution {
         }
         Map<Character, Integer> mapS = new HashMap<>();
         Map<Character, Integer> mapT = new HashMap<>();
-        int index = 0;
         for (int i = 0; i < s.length(); i++) {
             if (!mapS.containsKey(s.charAt(i))) {
-                mapS.put(s.charAt(i), index);
+                mapS.put(s.charAt(i), i);
             }
             if (!mapT.containsKey(t.charAt(i))) {
-                mapT.put(t.charAt(i), index);
+                mapT.put(t.charAt(i), i);
             }
             if (mapS.get(s.charAt(i)) != mapT.get(t.charAt(i))) {
                 return false;
             }
-            index++;
         }
         return true;
     }
